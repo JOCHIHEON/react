ecfe1bd1c2a21d00ec8d9374428e9301f4041824
JOCHIHEON <zhixian030@gmail.com>
2018-11-08 21:06:49 +0900

    init

---
+++ src/Nav.js
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Button } from 'reactstrap';
+import {
+    Collapse,
+    Navbar,
+    NavbarToggler,
+    NavbarBrand,
+    Nav,
+    NavItem,
+    NavLink,
+    UncontrolledDropdown,
+    Dropdown,
+    DropdownToggle,
+    DropdownMenu,
+    DropdownItem } from 'reactstrap';
+
+
+    export default class Example extends React.Component {
+    constructor(props) {
+      super(props);
+  
+      this.toggle = this.toggle.bind(this);
+      this.state = {
+        isOpen: false
+      };
+    }
+    toggle() {
+      this.setState({
+        isOpen: !this.state.isOpen
+      });
+    }
+    render() {
+      return (
+        <div>
+          <Navbar color="light" light expand="md">
+          <NavbarBrand href="/">REBOUND</NavbarBrand>
+          </Navbar>
+          <Navbar color="light" light expand="md">
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">Components</NavLink>
+                </NavItem>
+                <UncontrolledDropdown nav inNavbar>
+                  <DropdownToggle nav>
+                    Options
+                  </DropdownToggle>
+                  <DropdownMenu left>
+                    <DropdownItem>
+                      Option 1
+                    </DropdownItem>
+                    <DropdownItem>
+                      Option 2
+                    </DropdownItem>
+                    <DropdownItem divider />
+                    <DropdownItem>
+                      Reset
+                    </DropdownItem>
+                  </DropdownMenu>
+                </UncontrolledDropdown>
+                <NavItem>
+                <Button color="light">로그인</Button>
+                <Button color="light">회원가입</Button>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Navbar>
+        </div>
+      );
+    }
+  }
