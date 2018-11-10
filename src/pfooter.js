2e36eec5d874adaf89b4f0c921d309ea90f44c1c
kuwonpark1 <kuwonpark1@naver.com>
2018-11-10 17:37:27 +0900

    init

---
+++ src/pfooter.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './pfooter.css';
+import pft from './pft.js';
+
+class pfooter extends Component {
+  render() {
+    return (
+      <div className="pfooter">
+        <pft/>
+      </div>
+    );
+  }
+}
+
+export default pfooter;
