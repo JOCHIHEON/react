4a59681de15076b3245987274c8875d098afadb4
kuwonpark1 <kuwonpark1@naver.com>
2018-11-15 21:48:37 +0900

    sdsada

--- src/routes/board/MainFanBoard.js
+++ src/routes/board/MainFanBoard.js
@@ -13,16 +13,6 @@ const body = () => {
                     </Col>
                 </Row>
                 <Row>
-                    <Col><Link to = "/fanboard/fanboard">고양오리온스</Link></Col>
-                    <Col><Link to = "/fanboard/fanboard">부산KT</Link></Col>
-                    <Col><Link to = "/fanboard/fanboard">서울삼성</Link></Col>
-                    <Col><Link to = "/fanboard/fanboard">서울SK</Link></Col>
-                    <Col><Link to = "/fanboard/fanboard">안양KGC</Link></Col>
-                    <Col><Link to = "/fanboard/fanboard">인천전자랜드</Link></Col>
-                    <Col><Link to = "/fanboard/fanboard">울산현대모비스</Link></Col>
-                    <Col><Link to = "/fanboard/fanboard">원주DB</Link></Col>
-                    <Col><Link to = "/fanboard/fanboard">전주KCC</Link></Col>
-                    <Col><Link to = "/fanboard/fanboard">창원LG</Link></Col>
                     <Col><Link to = "/board/fan/">원주DB</Link></Col>
                     <Col><Link to = "/board/fan/">서울삼성</Link></Col>
                     <Col><Link to = "/board/fan/">서울SK</Link></Col>
